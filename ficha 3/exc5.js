c86f2fb8a59b58ca6c8f1198ac4f50a4334e6655
Luís Santos <luis.costasantos@outlook.com>
2019-03-22 11:37:26 +0000

    1

---
+++ ficha 3/exc5.js
@@ -0,0 +1,36 @@
+const frmGame = document.querySelector("#frmGame")
+
+frmGame.addEventListener("submit", function(event) {
+    const txtGameYear = document.querySelector("#txtGameYear").value
+
+    const currentYear = new Date().getFullYear()
+    if (txtGameYear > 1950 && txtGameYear <= currentYear ) {
+        alert("OK")
+        addGAmeTable()
+    }
+    else {
+        alert("erro")
+    }
+
+    event.preventDefault()
+
+
+
+})
+
+function addGAmeTable(){
+    const table = document.querySelector("table")
+    const gameName = document.querySelector("#txtGameName").value
+    const gameYear = document.querySelector("#txtGameYear").value
+    const gameGenre = document.querySelector("#txtGameGenre").value
+    const gameCover = document.querySelector("#txtGameCover").value
+
+    table.innerHTML += `
+        <tr>
+            <td>${gameName}</td>
+            <td>${gameYear}</td>
+            <td>${gameGenre}</td>
+            <td><img src="${gameCover}"/></td>
+            <td></td>
+    `
+}
